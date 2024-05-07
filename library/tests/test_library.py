4cee79ad77165f246330e0eb19ec4d7bb448c8b3
e537487 <maxime.derbigny@sbb.ch>
2024-05-07 14:07:21 +0200

    Ajout d'un test d'integration pour book_list

--- library/tests/test_library.py
+++ library/tests/test_library.py
@@ -19,3 +19,26 @@ class BookIntegrationTest(TestCase):
         book = Book.objects.get(id=1)
         assert book.author == "Dominique Roques"
         assert book.title == "Pico Bogue"
+
+
+
+    def test_book_list(self):
+            # Arrange : Création de quelques livres pour tester la liste des livres
+            Book.objects.create(title='Book 1', author='Author 1')
+            Book.objects.create(title='Book 2', author='Author 2')
+            Book.objects.create(title='Book 3', author='Author 3')
+
+            # Act : Envoi d'une requête GET pour obtenir la liste des livres
+            client = Client()
+            response = client.get(reverse('book_list'))
+
+            # Assert
+            # Vérifier que la réponse est un succès (status_code 200)
+            self.assertEqual(response.status_code, 200)
+            # Vérifier que tous les livres sont présents dans la réponse
+            self.assertContains(response, 'Book 1')
+            self.assertContains(response, 'Book 2')
+            self.assertContains(response, 'Book 3')
+            # Vérifier que le nombre de livres dans la réponse est correct
+            self.assertEqual(len(response.context['book_list']), 3)
+
